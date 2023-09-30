b835c347b7adf836a4aa9e85d987307658901bf1
ryanator20 <ryanator20@gmail.com>
2023-09-30 01:31:50 -0600

    Set up basic website frontend using nuxt and vue

---
+++ 2p-Website/pages/checkers.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Welcome to checkers!
+    </div>
+</template>
---
+++ 2p-Website/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <h1>
+            2 player games! Select a game below to begin, or input a game code
+        </h1>
+        <NuxtLink to="/checkers" class="game-icon"><img src="/checkers.jpg"></NuxtLink>
+    </div>
+</template>
+<style> 
+.game-icon {
+    width: 100px;
+    height: 100px;
+    border-radius: 999px;
+}
+.game-icon img {
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+}
+.home {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: #0cc5f7;
+}
+</style>
