8bfe484e766475b2d1bde418aff8d670b14ca00a
Ian Jackson <ianjack457@gmail.com>
2023-10-21 20:30:14 -0600

    Add /lobby router method, clean up and fix nil reference bug in /game router method

--- back-end/router.go
+++ back-end/router.go
@@ -19,44 +19,45 @@ func handleRoutes(router *gin.Engine, socket *melody.Melody) {
 		game := c.Param("game")
 		board, gameExists := types.NewGame(game)
 
-		//make sure the provided game parameter corresponds with a valid game type
-		if gameExists {
-			//if no id was given, create a new lobby
-			if id == "" {
-				lobby := types.NewLobby(game)
-				lobbyID := createLobbyID(c)
-
-				//loops until an unused lobby id is found
-				for _, ok := lobbies.Get(lobbyID); ok; _, ok = lobbies.Get(lobbyID) {
-					lobbyID = createLobbyID(c)
-				}
-
-				//add lobby with id to list and send id to frontend
-				lobbies.Set(lobbyID, &lobby)
-				c.String(http.StatusOK, lobbyID)
-			} else {
-				//if an id was given, try to find corresponding lobby in list
-				lobby, lobbyFound := lobbies.Get(id)
-				if lobby.GameType != game {
-					c.String(http.StatusOK, "Wrong game type")
-					return
-				}
-
-				//if lobby exists, create new board and send to frontend
-				if lobbyFound {
-					json_update, _ := json.Marshal(board)
-					c.String(http.StatusOK, string(json_update))
-				} else {
-					//if lobby doesn't exist, notify frontend
-					c.String(http.StatusOK, "Lobby not found")
-				}
-			}
-		} else {
+		if !gameExists {
 			//notify frontend if game parameter was invalid
 			c.String(http.StatusOK, game+" is not a supported game")
+			return
+		}
+
+		//if no id was given, create a new lobby
+		if id == "" {
+			lobby := types.NewLobby(game)
+			lobbyID := createLobbyID(c)
+
+			//loops until an unused lobby id is found
+			for _, ok := lobbies.Get(lobbyID); ok; _, ok = lobbies.Get(lobbyID) {
+				lobbyID = createLobbyID(c)
+			}
+
+			//add lobby with id to list and send id to frontend
+			lobbies.Set(lobbyID, &lobby)
+			c.String(http.StatusOK, lobbyID)
+		} else {
+			//if an id was given, try to find corresponding lobby in list
+			lobby, lobbyFound := lobbies.Get(id)
+
+			//send error msg if lobby doesn't exist or if game types are mistmatched
+			if !lobbyFound {
+				c.String(http.StatusOK, "Lobby not found")
+				return
+			} else if lobby.GameType != game {
+				c.String(http.StatusOK, "Wrong game type")
+				return
+			}
+
+			//create new board and send to frontend
+			json_update, _ := json.Marshal(board)
+			c.String(http.StatusOK, string(json_update))
 		}
 	})
 
+	//upgrades a clients http connection to a web socket
 	router.GET("/ws", func(c *gin.Context) {
 		lobbyID := c.Query("lobbyId")
 
@@ -68,7 +69,7 @@ func handleRoutes(router *gin.Engine, socket *melody.Melody) {
 
 		lobby, lobbyFound := lobbies.Get(lobbyID)
 
-		//return error msg is lobby doesn't exist of game is already in session
+		//return error msg is lobby doesn't exist or game is already in session
 		if !lobbyFound {
 			c.String(http.StatusOK, "Lobby not found")
 			return
@@ -80,7 +81,31 @@ func handleRoutes(router *gin.Engine, socket *melody.Melody) {
 		//upgrade http connection to web socket
 		socket.HandleRequest(c.Writer, c.Request)
 	})
-	router.Run(":8080")
+
+	//returns the type of game being hosted in a lobby
+	router.GET("lobby", func(c *gin.Context) {
+		lobbyID := c.Query("lobbyId")
+
+		//lobby id is required
+		if lobbyID == "" {
+			c.String(http.StatusOK, "No lobby ID")
+			return
+		}
+
+		lobby, lobbyFound := lobbies.Get(lobbyID)
+
+		//return error msg is lobby doesn't exist
+		if !lobbyFound {
+			c.String(http.StatusOK, "Lobby not found")
+			return
+		}
+
+		//return lobby game type
+		c.String(http.StatusOK, lobby.GameType)
+
+	})
+
+	router.Run(":8081")
 }
 
 func createLobbyID(c *gin.Context) string {
