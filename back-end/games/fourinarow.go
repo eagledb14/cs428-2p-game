63560587950cccfbd2fafa470cc5532b7c374c33
calhargis <calhargis1@gmail.com>
2023-11-15 17:05:59 -0700

    fixed fourinarow types usage

--- back-end/games/fourinarow.go
+++ back-end/games/fourinarow.go
@@ -1,6 +1,8 @@
 package games
 
 import (
+	"fmt"
+
 	"github.com/eagledb14/cs428-2p-game/types"
 )
 
@@ -12,100 +14,144 @@ const (
 	cols      = 7
 )
 
-func Fourinarow(lobby *types.Lobby) {
-	board := types.NewFourInARowBoard()
-	currentPlayer := 1
-	var row, col int
-
-	for {
-		move, quit := validateMsg(lobby)
-		if quit {
-			return
-		}
-
-		if move.Reset {
-			board := types.NewFourInARowBoard()
-			SendUpdate(lobby, board, currentPlayer, currentPlayer, true, false)
-			continue
-		}
-
-		row, col = move.To.X, move.To.Y
-
-		if isMoveValid(board, row, col, currentPlayer, move.Player) {
-			board.Set(row, col, currentPlayer)
-
-			if isGameOver(board, row, col, currentPlayer) {
-				nextPlayer := ToggleRandomPlayer(2)
-				SendUpdate(lobby, board, currentPlayer, nextPlayer, true, true)
-				currentPlayer = nextPlayer
-				continue
-			}
-
-			if isBoardFull(board) {
-				nextPlayer := ToggleRandomPlayer(2)
-				SendUpdate(lobby, board, -1, nextPlayer, true, true)
-				currentPlayer = nextPlayer
-				continue
-			}
-
-			SendUpdate(lobby, board, currentPlayer, togglePlayer(currentPlayer), true, false)
-			currentPlayer = togglePlayer(currentPlayer)
-		} else {
-			SendError(lobby, board, move, currentPlayer)
-		}
+func PrintBoard(b *types.Board) {
+	for _, row := range b.Board {
+		fmt.Println(row)
 	}
 }
 
-func (b *Board) PlacePiece(column, player int) bool {
-	if column < 0 || column >= cols || b.board[0][column] != emptyCell {
-		return false
+func Print(b *types.Board) {
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			value, err := b.Get(i, j)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			fmt.Print(value, " ")
+		}
+		fmt.Println()
+	}
+}
+
+func Fourinarow(lobby *types.Lobby) {
+	board := types.NewFourInARowBoard()
+	player := player1
+	winner := 0
+
+	fmt.Println("Welcome to Four-In-A-Row!")
+	PrintBoard(&board)
+
+	for winner == 0 {
+		fmt.Printf("Player %d, choose a column (0-6): ", player)
+		var col int
+		fmt.Scanln(&col)
+
+		if col < 0 || col >= cols {
+			fmt.Println("Invalid column. Choose a column between 0 and 6.")
+			continue
+		}
+		board, piecePlaced := PlacePiece(board, col, player)
+
+		if piecePlaced {
+			PrintBoard(&board)
+			if CheckWin(&board, player) {
+				winner = player
+			} else if player == player1 {
+				player = player2
+			} else {
+				player = player1
+			}
+		} else {
+			fmt.Println("Column is full. Choose another column.")
+		}
+	}
+
+	if winner > 0 {
+		fmt.Printf("Player %d wins!\n", winner)
+	} else {
+		fmt.Println("It's a draw. The game is over.")
+	}
+}
+
+func PlacePiece(board types.Board, column, player int) (types.Board, bool) {
+	rows, err := board.Get(0, 0)
+	if err != nil {
+		return board, false
+	}
+
+	if column < 0 || column >= rows {
+		return board, false
 	}
 
 	for row := rows - 1; row >= 0; row-- {
-		if b.board[row][column] == emptyCell {
-			b.board[row][column] = player
-			return true
+		value, err := board.Get(row, column)
+		if err != nil {
+			return board, false
+		}
+
+		if value == emptyCell {
+			board.Set(row, column, player)
+			return board, true
 		}
 	}
 
-	return false
+	return board, false
 }
 
-func (b *Board) CheckWin(player int) bool {
+func CheckWin(b *types.Board, player int) bool {
 	for row := 0; row < rows; row++ {
 		for col := 0; col < cols; col++ {
-			if b.board[row][col] == player {
+			cell, _ := b.Get(row, col)
+			if cell == player {
 				// Check horizontally
-				if col+3 < cols &&
-					b.board[row][col+1] == player &&
-					b.board[row][col+2] == player &&
-					b.board[row][col+3] == player {
-					return true
+				if col+3 < cols {
+					value1, err1 := b.Get(row, col+1)
+					value2, err2 := b.Get(row, col+2)
+					value3, err3 := b.Get(row, col+3)
+
+					if err1 == nil && err2 == nil && err3 == nil &&
+						value1 == player && value2 == player && value3 == player {
+						return true
+					}
 				}
 
 				// Check vertically
-				if row+3 < rows &&
-					b.board[row+1][col] == player &&
-					b.board[row+2][col] == player &&
-					b.board[row+3][col] == player {
-					return true
+				if row+3 < rows {
+					value1, err1 := b.Get(row+1, col)
+					value2, err2 := b.Get(row+2, col)
+					value3, err3 := b.Get(row+3, col)
+
+					if err1 == nil && err2 == nil && err3 == nil &&
+						value1 == player && value2 == player && value3 == player {
+						return true
+					}
 				}
 
 				// Check diagonally (up-right)
-				if row-3 >= 0 && col+3 < cols &&
-					b.board[row-1][col+1] == player &&
-					b.board[row-2][col+2] == player &&
-					b.board[row-3][col+3] == player {
-					return true
+				if row-3 >= 0 && col+3 < cols {
+					value1, err1 := b.Get(row-1, col+1)
+					value2, err2 := b.Get(row-2, col+2)
+					value3, err3 := b.Get(row-3, col+3)
+
+					if err1 == nil && err2 == nil && err3 == nil &&
+						value1 == player && value2 == player && value3 == player {
+						return true
+					}
 				}
 
 				// Check diagonally (up-left)
-				if row-3 >= 0 && col-3 >= 0 &&
-					b.board[row-1][col-1] == player &&
-					b.board[row-2][col-2] == player &&
-					b.board[row-3][col-3] == player {
-					return true
+				if row-3 >= 0 && col-3 >= 0 {
+					value1, err1 := b.Get(row-1, col-1)
+					value2, err2 := b.Get(row-2, col-2)
+					value3, err3 := b.Get(row-3, col-3)
+
+					if err1 == nil && err2 == nil && err3 == nil &&
+						value1 == player && value2 == player && value3 == player {
+						return true
+					}
 				}
+
 			}
 		}
 	}
